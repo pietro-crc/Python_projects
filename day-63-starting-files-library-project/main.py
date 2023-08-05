7f68166e07e8d0a20df0aa4be6e94e2ad66bb300
pietro-crc <135025750+pietro-crc@users.noreply.github.com>
2023-08-05 15:09:38 +0200

    Update main.py

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -39,19 +39,6 @@ def add_book(title,author,rating):
 
 
 
-    '''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 
 
 all_books = []
